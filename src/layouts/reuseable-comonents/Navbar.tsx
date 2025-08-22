651042567479a816d93fd7eddac088a4679dc321
Sardar-Mukhshaf <168126016+sardar-mukhshaf@users.noreply.github.com>
2025-08-22 16:21:05 +0500

    solving auth problems, based on role access & storing token

--- src/layouts/reuseable-comonents/Navbar.tsx
+++ src/layouts/reuseable-comonents/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useLogout } from '../authLayouts/hooks/useAuthData';
 
